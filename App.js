2c1049b5daa347572e8250e6a1c93e84e0f20b30
lijiahao <lijiahao@cmschina.com.cn>
2018-06-20 22:37:35 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import codePush from "react-native-code-push";
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+class App extends Component<Props> {
+
+  componentDidMount(){
+    codePush.sync({
+      updateDialog: true,
+      installMode: codePush.InstallMode.IMMEDIATE,
+      mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+      deploymentKey: 'Pay3W7Nfaa3bLG9C9ZXTwMgS7zMD68d21987-8919-4d4e-8062-52c8293250cb',
+      });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default App;
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # RnHotUpdateDemo
-A react-native-code-push demo，easy to understand.
+一个简单的react-native-code-push demo，可根据http://geocld.github.io/2018/06/19/react-native-code-push/进行实际练习。
+
+涉及的依赖及环境版本如下:
+
+React-native: 0.55.4
+
+React-native-code-push: 5.3.4
+
+Xcode: 9.4
+
+macOS：10.13.4
+
+
+
+目前仅实现iOS端的热更新，android端的后续更新。
